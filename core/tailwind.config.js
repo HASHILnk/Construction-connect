fb2ba28e600032579e36d5d71aa0d68de6de4cef
Hashil N K <mohammedhashil2018@gmail.com>
2025-11-29 12:24:32 +0530

    Updated Tailwind setup, base.html, and admin dashboard

---
+++ core/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./core/templates/**/*.{html,js}",
+    "./accounts/templates/**/*.{html,js}",
+    "./profiles/templates/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
